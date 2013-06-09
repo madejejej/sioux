8eb97b6fd54359c9ec704b7142cbea5df25510cd
s1mplex <kacperoza@gmail.com>
2013-06-09 17:26:27 +0200

    Moar tests

--- test/request_parser_test.cpp
+++ test/request_parser_test.cpp
@@ -10,12 +10,19 @@ namespace sioux {
 	namespace test {
 
 	request_parser req_parser;
-	std::string basic_request("GET /index.html HTTP/1.0\r\n\r\n");
+	std::string basic_request( "GET /index.html HTTP/1.0\r\n\r\n" );
 
-	TEST(parse, shouldUnderstandGetRequest) {
+	TEST(parse, GetRequestCase) {
 		auto req = req_parser.parse(std::make_shared<std::string>(basic_request));
 		ASSERT_EQ("GET", req->method);
 		ASSERT_EQ("index.html", req->uri);
+		ASSERT_EQ("HTTP/1.0", req->http_version);
+	}
+
+	std::string bad_request( "LOL nowwhere/really_nowhere.wtf HTTP/1.0" );
+	TEST(parse, BadRequestCase) {
+		auto req = req_parser.parse(std::make_shared<std::string>(bad_request));
+		ASSERT_EQ( nullptr, req.get() );
 	}
 
 
